cdf22a8f74f904df8d3e81aae87307e503098601
Ben Hardy <benhardy@Bens-MacBook-Pro-3.local>
2014-12-23 09:09:15 -0600

    Added the ability to specify flags to inputs. Useful for if you want the image to be grayscale.

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ import numpy
 import sys
 
 print("Click the image and press a key to exit.\n")
-if len(sys.argv) == 1:
+if len(sys.argv) == 1: # If there are no arguments, just load the default image
     img = cv2.imread('what.png')
-elif len(sys.argv) > 1:
+elif len(sys.argv) == 2: # load image at path with default imread val
     img = cv2.imread(sys.argv[1])
-cv2.imshow('result', img)
+elif len(sys.argv) == 3: # load image using flag. Useful for opening grayscale
+    img = cv2.imread(sys.argv[1], int(sys.argv[2]))
+cv2.imshow('result', img) # show the result
 
-cv2.waitKey(0)
+cv2.waitKey(0) # listen for a key press
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() # upon hearing key press, close the image window
