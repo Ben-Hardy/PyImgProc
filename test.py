992604ed6b10b4d45046059555ab8a4f8d990cd1
Ben Hardy <benhardy@Bens-MacBook-Pro-3.local>
2014-12-23 09:00:54 -0600

    Oops forgot to commit new test.py. I'm used to stupid SVN where it commits everything unless you tell it to.

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
+'''
+Test program to make sure cv2 and numpy are working. 
+Based on a simple tutorial. Run this before trying anything else
+to make sure your setup is function. This is all Python 2.7.
+
+By Ben Hardy
+'''
+
 import cv2
 import numpy
+import sys
 
-print("Press ESC to exit.\n")
-
-img = cv2.imread('aurora.png')
-
+print("Click the image and press a key to exit.\n")
+if len(sys.argv) == 1:
+    img = cv2.imread('what.png')
+elif len(sys.argv) > 1:
+    img = cv2.imread(sys.argv[1])
 cv2.imshow('result', img)
-r = cv2.waitKey(0) & 0xFF
 
-if r == 27:
-	cv2.destroyAllWindows()
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
